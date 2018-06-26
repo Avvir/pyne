694604db8ba5c2f4c986134ca90056b38657848f
Tira Odhner <atiraodhner@gmail.com>
2018-06-26 13:14:19 -0400

    add some more expectation helpers.

--- tests/test_helpers/matchers.py
+++ tests/test_helpers/matchers.py
@@ -2,15 +2,44 @@ from termcolor import cprint
 
 
 class Matchers:
-    def __init__(self, actual):
-        self.actual = actual
-        self.to_be_matcher = Matcher("to_be", lambda actual, expected: (actual == expected))
+    def __init__(self, subject):
+        self.subject = subject
+        self._to_be_matcher = Matcher("to_be", lambda actual, expected: (actual == expected))
 
     def to_be(self, expected):
-        self.to_be_matcher.match(self.actual, expected)
+        self._to_be_matcher.match(self.subject, expected)
 
     def not_to_be(self, expected):
-        InverseMatcher(self.to_be_matcher).match(self.actual, expected)
+        InverseMatcher(self._to_be_matcher).match(self.subject, expected)
+
+    def to_have_length(self, length):
+        self._to_have_length_matcher = Matcher("to_have_length", lambda subject, length: (len(subject) == length))
+        self._to_have_length_matcher.match(self.subject, length)
+
+    def to_raise_error_message(self, message):
+        RaiseMatcher().match(self.subject, message)
+
+class RaiseMatcher:
+    def __init__(self):
+        self.expected_message = None
+        self.actual_exception = None
+
+    def check_error(self, method, message):
+        self.expected_message = message
+        try:
+            method()
+        except Exception as e:
+            self.actual_exception = e
+            return e.args[0] == message
+
+    def message_format(self):
+        if self.actual_exception is None:
+            return "Expected ({subject}) to raise an exception with message ({0}) but no exception was raised"
+        else:
+            return "Expected ({subject}) to raise an exception with message ({0}) but message was " + self.actual_exception.args[0]
+
+    def match(self, subject, *params):
+        return Matcher("to_raise_error_message", self.check_error, self.message_format).match(subject, *params)
 
 
 class InverseMatcher:
@@ -18,15 +47,15 @@ class InverseMatcher:
         self.matcher = matcher
         self.message_format = message_format
 
-    def match(self, actual, *params):
-        if self.matcher.comparator(actual, *params):
-            message_format = self.default_message() if self.message_format is None else self.message_format
-            message = message_format.format(actual=actual, *params)
+    def match(self, subject, *params):
+        if self.matcher.comparator(subject, *params):
+            message_format = self.default_message() if self.message_format is None else self.message_format()
+            message = message_format.format(subject=subject, *params)
             cprint("\n" + message + "\n", 'yellow')
             raise Exception(message)
 
     def default_message(self):
-        return "Expected ({actual}) not " + " ".join(self.name.split("_")) + " ({0})"
+        return "Expected ({subject}) not " + " ".join(self.name.split("_")) + " ({0})"
 
 
 class Matcher:
@@ -35,16 +64,16 @@ class Matcher:
         self.comparator = comparator
         self.message_format = message_format
 
-    def match(self, actual, *params):
-        if not self.comparator(actual, *params):
-            message_format = self.default_message() if self.message_format is None else self.message_format
-            message = message_format.format(actual=actual, *params)
+    def match(self, subject, *params):
+        if not self.comparator(subject, *params):
+            message_format = self.default_message() if self.message_format is None else self.message_format()
+            message = message_format.format(subject=subject, *params)
             cprint("\n" + message + "\n", 'yellow')
             raise Exception(message)
 
     def default_message(self):
-        return "Expected ({actual}) " + " ".join(self.name.split("_")) + " ({0})"
+        return "Expected ({subject}) " + " ".join(self.name.split("_")) + " ({0})"
 
 
-def expect(actual):
-    return Matchers(actual)
+def expect(subject):
+    return Matchers(subject)
---
+++ tests/test_helpers/matchers_test.py
@@ -0,0 +1,62 @@
+from .matchers import expect
+import re
+
+def test__to_be__can_pass():
+    expect(1).to_be(1)
+
+
+def test__to_be__fails_with_message():
+    exception = None
+    try:
+        expect(1).to_be(2)
+    except Exception as e:
+        exception = e
+    finally:
+        assert "Expected (1) to be (2)" == exception.args[0]
+
+
+def test__to_have_length__can_pass():
+    expect([123, 123, 123]).to_have_length(3)
+
+
+def test__to_have_length__fails_with_message():
+    exception = None
+    try:
+        expect([123, 123, 123]).to_have_length(4)
+    except Exception as e:
+        exception = e
+    finally:
+        assert "Expected ([123, 123, 123]) to have length (4)" == exception.args[0]
+
+
+def test__to_raise_error_message__can_pass():
+    def error_method():
+        raise Exception("some message")
+
+    expect(error_method).to_raise_error_message("some message")
+
+
+def test__to_raise_error_message__can_fail_because_the_message_is_wrong():
+    def error_method():
+        raise Exception("some other message")
+
+    error = None
+    try:
+        expect(error_method).to_raise_error_message("some message")
+    except Exception as e:
+        error = e
+    finally:
+        assert re.search("but message was", error.args[0])
+
+
+def test__to_raise_error_message__can_fail_because_no_error_is_raised():
+    def error_method():
+        pass
+
+    error = None
+    try:
+        expect(error_method).to_raise_error_message("some message")
+    except Exception as e:
+        error = e
+    finally:
+        assert re.search("but no exception was raised", error.args[0])
