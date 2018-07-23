1551119fafbd88755954193ae5ba3d73454b4711
Tira Odhner <atiraodhner@gmail.com>
2018-07-22 21:24:45 -0400

    make a two-test fixture.

--- tests/test_helpers/test_resource_paths.py
+++ tests/test_helpers/test_resource_paths.py
@@ -4,5 +4,6 @@ from os.path import abspath
 project_root = abspath(path.join(path.dirname(path.abspath(__file__)), "..", ".."))
 
 
-cli_test_fixture_path = path.join(project_root, "examples", "cli_test_fixture")
+cli_test_fixture_path = path.join(project_root, "examples", "cli_single_test_fixture")
+cli_two_file_test_fixture_path = path.join(project_root, "examples", "cli_two_file_test_fixture")
 pyne_path = path.join(project_root, "pyne")
