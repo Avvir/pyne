cc81aac5b02a74768bfee63a7e0a34ad9cb14f49
tsande01 <tsande01@gmail.com>
2018-12-04 14:42:12 -0500

    Ignore hidden dirs (#4)
    * Ignore hidden subdirectories when looing for tests
    
    * Add test for ignoring hidden subdirectory

---
+++ examples/cli_hidden_file_test_fixture/.hidden_dir/tests/some_ignored_file_test.py
@@ -0,0 +1,29 @@
+from pyne.pyne_test_collector import describe, fdescribe, fit, it
+from pyne.pyne_tester import pyne
+
+
+@pyne
+def some_focusing_test():
+    @fit("can be focused")
+    def _(self):
+        pass
+
+    @fit("can be focused")
+    def _(self):
+        pass
+
+    @it("can be ignored")
+    def _(self):
+        pass
+
+    @fdescribe("a focused block")
+    def _():
+        @it("runs its children")
+        def _(self):
+            pass
+
+        @describe("its grandchildren")
+        def _():
+            @it("also run")
+            def _(self):
+                pass
