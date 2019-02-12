66c39d1d1160ebed9c3328cd783a8db4903e7317
Cody J. Phillips <cody@avvir.io>
2019-02-12 17:21:19 -0500

    Create spy function that actually calls spied upon method

--- pyne/test_doubles/spy.py
+++ pyne/test_doubles/spy.py
@@ -1,12 +1,15 @@
 class Spy:
-    def __init__(self, stubbed_object=None, method=None):
+    def __init__(self, stubbed_object=None, method=None, call_method=True):
         self.method = method
         self.stubbed_object = stubbed_object
         self.last_call = None
         self.return_value = None
+        self.call_method = call_method
 
     def __call__(self, *args, **kwargs):
         self.last_call = (args, kwargs)
+        if self.call_method:
+            self.return_value = self.method(*args, **kwargs)
         return self.return_value
 
     def returns(self, return_value):
@@ -20,3 +23,12 @@ class Spy:
                 setattr(self.stubbed_object, self.method.__name__, self.method)
             else:
                 self.stubbed_object.__setattr__(self.method.__name__, self.method)
+
+def spy(object_to_stub, method):
+    spy = Spy(object_to_stub, method, call_method=True)
+
+    if isinstance(object_to_stub, type):
+        setattr(object_to_stub, method.__name__, spy)
+    else:
+        object_to_stub.__setattr__(method.__name__, spy)
+    return spy
--- pyne/test_doubles/stub.py
+++ pyne/test_doubles/stub.py
@@ -2,7 +2,7 @@ from pyne.test_doubles.spy import Spy
 
 
 def stub(object_to_stub, method):
-    spy = Spy(object_to_stub, method)
+    spy = Spy(object_to_stub, method, call_method=False)
 
     if isinstance(object_to_stub, type):
         setattr(object_to_stub, method.__name__, spy)
