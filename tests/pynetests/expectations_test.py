854ec7be56817e5589f217fd9a723c61dd74175a
Tira Odhner <atiraodhner@gmail.com>
2019-02-16 21:13:36 -0500

    Tests: rename some tests.

--- tests/pynetests/expectations_test.py
+++ tests/pynetests/expectations_test.py
@@ -1,6 +1,6 @@
 """Pyne tests for pyne.expectations"""
 from pyne.expectations import expect
-from pyne.pyne_test_collector import it, describe, fdescribe
+from pyne.pyne_test_collector import it, describe
 from pyne.pyne_tester import pyne
 
 # flake8: noqa
@@ -19,15 +19,15 @@ def expect_assertion_error(fun):
 
 @pyne
 def expectations_tests():
-    @describe("When we expect an error to be raised a message")
+    @describe("#to_raise_error_with_message")
     def _():
-        @it("it passes when such a message is raised.")
+        @it("passes when such a message is raised.")
         def _(self):
             def good_fun():
                 raise Exception("Matching message!")
             expect(good_fun).to_raise_error_with_message("Matching message!")
 
-        @it("it fails when a different message is raised.")
+        @it("fails when a different message is raised.")
         def _(self):
             def bad_fun():
                 raise Exception("Non-matching message!")
@@ -37,7 +37,7 @@ def expectations_tests():
                     "Matching message!")
             expect_assertion_error(test_fun)
 
-        @it("it fails when no error is raised.")
+        @it("fails when no error is raised.")
         def _(self):
             def bad_fun():
                 pass
@@ -47,24 +47,26 @@ def expectations_tests():
                     "Matching message!")
             expect_assertion_error(test_fun)
 
-    @describe("When we expect an error of a type to be raised")
+    @describe("#to_raise_error_of_type")
     def _():
-        @it("it passes when such a message is raised.")
+        @it("can pass")
         def _(self):
             def good_fun():
                 raise StopIteration("Bad stuff happened.")
             expect(good_fun).to_raise_error_of_type(StopIteration)
 
-        @it("it fails when a different type is raised.")
-        def _(self):
-            def bad_fun():
-                raise StopIteration("Bad stuff happended.")
+        @describe("when a different type of error is raised")
+        def _():
+            @it("fails")
+            def _(self):
+                def bad_fun():
+                    raise StopIteration("Bad stuff happended.")
 
-            def test_fun():
-                expect(bad_fun).to_raise_error_of_type(IOError)
-            expect_assertion_error(test_fun)
+                def test_fun():
+                    expect(bad_fun).to_raise_error_of_type(IOError)
+                expect_assertion_error(test_fun)
 
-        @it("it fails when no error is raised.")
+        @it("fails when no error is raised.")
         def _(self):
             def bad_fun():
                 pass
@@ -73,46 +75,42 @@ def expectations_tests():
                 expect(bad_fun).to_raise_error_of_type(StopIteration)
             expect_assertion_error(test_fun)
 
-    @describe("When we check for None")
+    @describe("#to_be_none")
     def _():
-        @it("it passes if the value is None")
+        @it("passes if the value is None")
         def _(self):
             expect(None).to_be_none()
 
-        @it("it fails if the value is not None")
+        @it("fails if the value is not None")
         def _(self):
             expect_assertion_error(lambda: expect(5).to_be_none())
 
-    @describe("When we check for not None")
+    @describe("#not_to_be_none")
     def _():
-        @it("it passes if the value is not None")
+        @it("passes when the value is not None")
         def _(self):
             expect(5).not_to_be_none()
 
-        @it("it fails if the value is None")
+        @it("fails when the value is None")
         def _(self):
             expect_assertion_error(lambda: expect(None).not_to_be_none())
 
-    @describe("When we check for a collection to contain an element")
+    @describe("#to_contain")
     def _():
-        @it("it passes if the element is in the set")
+        @it("can pass for an element in a set")
         def _(self):
-            test_set = set([5, 10, 3])
-            expect(test_set).to_contain(5)
+            expect({5, 10, 3}).to_contain(5)
 
-        @it("it fails if the element is not in the set")
+        @it("fails if the element is not in the set")
         def _(self):
-            test_set = set([5, 10, 3])
-            expect_assertion_error(lambda: expect(test_set).to_contain(1000))
+            expect_assertion_error(lambda: expect({5, 10, 3}).to_contain(1000))
 
-    @describe("When we check for a collection to not contain an element")
+    @describe("#not_to_contain")
     def _():
         @it("it passes if the element is not in the set")
         def _(self):
-            test_set = set([5, 10, 3])
-            expect(test_set).not_to_contain(100)
+            expect({5, 10, 3}).not_to_contain(100)
 
         @it("it fails if the element is in the set")
         def _(self):
-            test_set = set([5, 10, 3])
-            expect_assertion_error(lambda: expect(test_set).not_to_contain(5))
+            expect_assertion_error(lambda: expect({5, 10, 3}).not_to_contain(5))
