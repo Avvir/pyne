29b9a4cb2976524b76e22ab5410827e9fe65d31e
Cody J. Phillips <codyp@cis.upenn.edu>
2019-03-17 12:26:13 -0400

    WIP: Need to store file level imports, reload them before pyne block runs

--- examples/cli_module_file_isolation_test_fixture/tests/some_file_a_test.py
+++ examples/cli_module_file_isolation_test_fixture/tests/some_file_a_test.py
@@ -2,14 +2,10 @@ from pyne.expectations import expect
 from pyne.pyne_test_collector import it
 from pyne.pyne_tester import pyne
 import sys
-from tests.test_helpers.test_modules import some_module_in_file_a
-from tests.test_helpers.test_modules import some_shared_module
-
+from tests.test_helpers.test_modules import some_module_in_file_a, some_shared_module
 
 @pyne
 def some_first_file_test():
-    from tests.test_helpers.test_modules import some_module_in_pyne_a
-
     @it("can pass")
     def _(self):
         pass
@@ -19,14 +15,12 @@ def some_first_file_test():
         from tests.test_helpers.test_modules import some_module_in_it_a
         loaded_modules_with_some = { name: module for (name, module) in sys.modules.items() if "some" in name }
         expect(loaded_modules_with_some).to_contain("tests.test_helpers.test_modules.some_module_in_file_a")
-        expect(loaded_modules_with_some).to_contain("tests.test_helpers.test_modules.some_module_in_pyne_a")
         expect(loaded_modules_with_some).to_contain("tests.test_helpers.test_modules.some_module_in_it_a")
 
     @it("has no modules from b")
     def _(self):
         loaded_modules_with_some = { name: module for (name, module) in sys.modules.items() if "some" in name }
         expect(loaded_modules_with_some).not_to_contain("tests.test_helpers.test_modules.some_module_in_file_b")
-        expect(loaded_modules_with_some).not_to_contain("tests.test_helpers.test_modules.some_module_in_pyne_b")
         expect(loaded_modules_with_some).not_to_contain("tests.test_helpers.test_modules.some_module_in_it_b")
 
     @it("shared module has changes from a and not b")
--- examples/cli_module_file_isolation_test_fixture/tests/some_file_b_test.py
+++ examples/cli_module_file_isolation_test_fixture/tests/some_file_b_test.py
@@ -10,21 +10,17 @@ from tests.test_helpers.test_modules import some_module_in_file_b, some_shared_m
 
 @pyne
 def some_second_file_test():
-    from tests.test_helpers.test_modules import some_module_in_pyne_b
-
     @it("has all the modules from b")
     def _(self):
         from tests.test_helpers.test_modules import some_module_in_it_b
         loaded_modules_with_some = { name: module for (name, module) in sys.modules.items() if "some" in name }
         expect(loaded_modules_with_some).to_contain("tests.test_helpers.test_modules.some_module_in_file_b")
-        expect(loaded_modules_with_some).to_contain("tests.test_helpers.test_modules.some_module_in_pyne_b")
         expect(loaded_modules_with_some).to_contain("tests.test_helpers.test_modules.some_module_in_it_b")
 
     @it("has no modules from a")
     def _(self):
         loaded_modules_with_some = { name: module for (name, module) in sys.modules.items() if "some" in name }
         expect(loaded_modules_with_some).not_to_contain("tests.test_helpers.test_modules.some_module_in_file_a")
-        expect(loaded_modules_with_some).not_to_contain("tests.test_helpers.test_modules.some_module_in_pyne_a")
         expect(loaded_modules_with_some).not_to_contain("tests.test_helpers.test_modules.some_module_in_it_a")
 
     @it("shared module has changes from b and not a")
