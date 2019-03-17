c13c410141ec44e2a45b351315ce8abab7abf649
Cody J. Phillips <codyp@cis.upenn.edu>
2019-03-17 10:28:29 -0400

    Add failing test for module file isolation - running pyne tests from cli should have the same behavior as running each file independently - modules should be reinitialized between pyne test files - any module imports in pyne file A should not be seen in B and vice versa - any module changes in pyne file A should not be seen in B and vice versa

--- tests/test_helpers/test_resource_paths.py
+++ tests/test_helpers/test_resource_paths.py
@@ -9,5 +9,7 @@ cli_two_file_test_fixture_path = path.join(project_root, "examples", "cli_two_fi
 cli_nested_directory_tests_fixture_path = path.join(project_root, "examples", "nested_directory_tests_fixture")
 cli_focused_test_fixture_path = path.join(project_root, "examples", "cli_focused_test_fixture")
 cli_excluded_tests_fixture_path = path.join(project_root, "examples", "cli_excluded_tests_fixture")
+cli_module_file_isolation_test_fixture_path = path.join(project_root, "examples", "cli_module_file_isolation_test_fixture")
+cli_globals_file_isolation_test_fixture_path = path.join(project_root, "examples", "cli_globals_file_isolation_test_fixture")
 cli_hidden_file_path = path.join(project_root, "examples", "cli_hidden_file_test_fixture")
 pyne_path = path.join(project_root, "pyne")
