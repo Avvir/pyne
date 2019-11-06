9aa767ae92865c15271002e3724df62fe9af08bc
Robin Thomas <robin@avvir.io>
2019-11-06 11:45:01 -0500

    Pine trees means passing; pine trees with fire means failures (#5)
    * red X when failures; pine trees only when green; plus gitignore python eggs and egg-info
    
    * factored out emoji-decorating from results reporters into function;
    use different emoji decor when there are test failures; conform
    all emoji-decorated message lines to pep8 line width of 72 chars.
    
    * fixed tab/space mix (finally got .vimrc correct)
    
    * picked up stray reporting msg that used the Pine emoji as literal;
    changed it to use FAIL_DECOR with the fire+pine
    
    * changed logic to favor the left side for message decor in the corner
    case of space being available for only *one* unit of decor; in that
    case, better to have it on the left.

--- pynetest/lib/result_reporters/pyne_result_reporters.py
+++ pynetest/lib/result_reporters/pyne_result_reporters.py
@@ -6,6 +6,23 @@ from termcolor import colored
 from pynetest.lib.result_reporters.printing_reporter import PrintingReporter
 from pynetest.lib.result_reporters.record_for_summary_reporter import RecordForSummaryReporter
 
+SUCCESS_DECOR = '🌲'
+FAIL_DECOR = '🔥🌲'
+
+def decorate_message_line(msg, decor, line_width=72):
+    # one space char on either side of msg
+    space_for_decor = line_width - ( len(msg) + 2 )
+    if space_for_decor < len(decor):
+        return msg
+    decor_units = space_for_decor // len(decor)
+    right_decor_units = decor_units // 2
+    left_decor_units = decor_units - right_decor_units
+    return (
+        (decor * left_decor_units) + 
+        ' ' + msg + ' ' + 
+        (decor * right_decor_units)
+    )
+
 
 class PyneStats:
     def __init__(self):
@@ -77,20 +94,21 @@ class PyneStatSummaryReporter(StatTrackingReporter):
     def report_end_result(self):
         StatTrackingReporter.report_end_result(self)
 
+        decor = FAIL_DECOR if self.stats.failure_count > 0 else SUCCESS_DECOR
         if self.stats.test_count == 0:
-            stats = '\n🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲 Ran 0 tests 🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲'
+            stats = '\n' + decorate_message_line('Ran 0 tests', decor)
         else:
             failures = "{0} failed, ".format(self.stats.failure_count) if self.stats.failure_count > 0 else ""
             pendings = ", {0} pending".format(self.stats.pending_count) if self.stats.pending_count > 0 else ""
 
-            stats = '\n🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲 ' \
-                    '{failures}{pass_count} passed{pendings} in {seconds:0.2f} seconds' \
-                    ' 🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲' \
+            stat_msg =  '{failures}{pass_count} passed{pendings} in {seconds:0.2f} seconds' \
                 .format(
                 pendings=pendings,
                 failures=failures,
                 pass_count=self.stats.pass_count,
                 seconds=self.stats.total_timing_millis / 1000)
+
+            stats = '\n' + decorate_message_line(stat_msg, decor)
 
         return self._printable(stats)
 
@@ -151,7 +169,8 @@ class PyneFailureSummaryReporter(StatTrackingReporter):
         StatTrackingReporter.report_failure(self, failed_behavior, it_block, filtered_exception, timing_millis)
         full_description = it_block.description
         self.failure_messages.append(
-            colored("🌲 Failure: \"{full_description}\" in <{behavior_description}> ", 'red', None, ['bold']).format(
+            colored("{decor} Failure: \"{full_description}\" in <{behavior_description}> ", 'red', None, ['bold']).format(
+                decor=FAIL_DECOR,
                 full_description=full_description,
                 behavior_description=failed_behavior.description))
         self.failure_messages.append(filtered_exception)
