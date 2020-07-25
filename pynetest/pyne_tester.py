25acb90184205837ed4ca9b17c354b1c3b723848
Cody J. Phillips <cody@avvir.io>
2020-07-25 08:57:58 -0400

    Suppress trackback print of TestFailureException
    When running a single pyne test, there is nothing to catch the
    TestFailureException as happens in the cli tool. Instead, it gets
    printed without formatting and makes the test results hard to read.
    We can make it look cleaner by only printing the exception value.
    This is done by overriding sys.excepthook and adding special printing
    logic for TestFailureException.

--- pynetest/pyne_tester.py
+++ pynetest/pyne_tester.py
@@ -1,9 +1,20 @@
 from pynetest.pyne_config import config
-from pynetest.lib.result_reporters.pyne_result_reporters import reporter
+from pynetest.lib.result_reporters.pyne_result_reporters import reporter, TestFailureException
 from pynetest.lib.pyne_test_blocks import DescribeBlock
 from pynetest.pyne_test_collector import test_collection
 from pynetest.pyne_test_runner import run_tests
 
+
+def replace_excepthook():
+    import sys
+    original_excepthook = sys.excepthook
+    def new_excepthook(type, value, traceback):
+        if type == TestFailureException:
+            print(value)
+            return
+        else:
+            original_excepthook(type, value, traceback)
+    sys.excepthook = new_excepthook
 
 def pyne(tests_method):
     if config.report_between_suites:
@@ -13,3 +24,5 @@ def pyne(tests_method):
     if config.report_between_suites:
         run_tests(describe_block, reporter)
     return tests_method
+
+replace_excepthook()
