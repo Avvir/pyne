e0849ce21f14412b087689de867b2374bafa7a7d
Trevor <trevor@avvir.io>
2022-10-07 15:09:33 -0400

    Add full test heritage to failures list

--- tests/lib/result_reporters/pyne_failures_list_reporter_test.py
+++ tests/lib/result_reporters/pyne_failures_list_reporter_test.py
@@ -13,9 +13,10 @@ def test__report_brief_description_for_all_failures():
         try:
             raise exception
         except Exception as e:
-            describe_block = DescribeBlock(None, "When description", None)
+            top_describe_block = DescribeBlock(None, "When Top describe", None)
+            describe_block = DescribeBlock(top_describe_block, "When description", None)
             block = ItBlock(describe_block, "It does", None)
             reporter.report_failure(block, block, e, 0)
         reporter.report_end_result()
 
-    expect(printed_text).to_contain(contains("When description: It does"))
+    expect(printed_text).to_contain(contains("When Top describe >> When description >> It does"))
